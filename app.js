482d9f2c2dc66703a451871cc004e9c6b2477612
“nagoorgani” <ynagoorgani@gmail.com>
2023-03-24 23:06:09 +0530

    On branch main  Your branch is up to date with 'origin/main'.
     Changes to be committed:
    	modified:   app.js
    	renamed:    images/2.png -> image/2.PNG
    	renamed:    images/3.png -> image/3.PNG
    	renamed:    images/4.png -> image/4.PNG
    	renamed:    images/5.png -> image/5.PNG
    	renamed:    images/6.png -> image/6.PNG
    	renamed:    images/shopping.svg -> image/shopping.svg
    	modified:   index.html
    	modified:   style.css

--- app.js
+++ app.js
@@ -6,12 +6,13 @@ let body = document.querySelector('body');
 let total = document.querySelector('.total');
 let quantity = document.querySelector('.quantity');
 
-openShopping.addEventListener('click', () => {
+openShopping.addEventListener('click', ()=>{
     body.classList.add('active');
 })
 closeShopping.addEventListener('click', ()=>{
     body.classList.remove('active');
 })
+
 let products = [
     {
         id: 1,
@@ -50,3 +51,58 @@ let products = [
         price: 120000
     }
 ];
+let listCards  = [];
+function initApp(){
+    products.forEach((value, key) =>{
+        let newDiv = document.createElement('div');
+        newDiv.classList.add('item');
+        newDiv.innerHTML = `
+            <img src="image/${value.image}">
+            <div class="title">${value.name}</div>
+            <div class="price">${value.price.toLocaleString()}</div>
+            <button onclick="addToCard(${key})">Add To Card</button>`;
+        list.appendChild(newDiv);
+    })
+}
+initApp();
+function addToCard(key){
+    if(listCards[key] == null){
+        listCards[key] = products[key];
+        listCards[key].quantity = 1;
+    }
+    reloadCard();
+}
+function reloadCard(){
+    listCard.innerHTML = '';
+    let count = 0;
+    let totalPrice = 0;
+    listCards.forEach((value, key)=>{
+        totalPrice = totalPrice + value.price;
+        count = count + value.quantity;
+        if(value != null){
+            let newDiv = document.createElement('li');
+            newDiv.innerHTML = `
+                <div><img src="image/${value.image}"/></div>
+                <div>${value.name}</div>
+                <div>${value.price.toLocaleString()}</div>
+                <div>
+                    <button onclick="changeQuantity(${key}, ${value.quantity - 1})">-</button>
+                    <div class="count">${value.quantity}</div>
+                    <button onclick="changeQuantity(${key}, ${value.quantity + 1})">+</button>
+                </div>`;
+                listCard.appendChild(newDiv);
+        }
+    })
+    total.innerText = totalPrice.toLocaleString();
+    quantity.innerText = count;
+}
+function changeQuantity(key, quantity){
+    console.log(key, quantity);
+    if(quantity == 0){
+        delete listCards[key];
+    }else{
+        listCards[key].quantity = quantity;
+        listCards[key].price = quantity * products[key].price;
+    }
+    reloadCard();
+}
